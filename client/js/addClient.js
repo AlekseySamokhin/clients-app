4c6dd3f92d4adf74934950a3b6c94dd97b54453e
AlekseySamokhin <lel.dude01@gmail.com>
2022-04-29 00:21:29 +0300

    fefactoring

---
+++ client/js/addClient.js
@@ -0,0 +1,66 @@
+import { createClientsFofm, createElement } from "./index.js";
+
+export const addClientModal = () => {
+  const createForm = createClientsFofm();
+  const modal = createElement("div", "modal", "site-modal", "modal-active");
+  const modalContent = createElement(
+    "div",
+    "modal__content",
+    "site-modal__content",
+    "modal-active"
+  );
+  createForm.form.classList.add("add-client");
+
+  modal.append(modalContent);
+  modalContent.append(
+    createForm.modalClose,
+    createForm.modalTitle,
+    createForm.form
+  );
+
+  // add Client
+  createForm.form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let contacts = [];
+    let clientObj = [];
+
+    let genderInput = document.getElementsByName("gender");
+    let genderValue;
+    for (var i = 0; i < genderInput.length; i++) {
+      if (genderInput[i].checked) {
+        genderValue = gender[i].value;
+      }
+    }
+
+    let educationValue;
+    let educationInput = document.getElementById("floatingEducation");
+    if (educationInput.checked) {
+      educationValue = "Есть";
+    } else {
+      educationValue = "Нет";
+    }
+
+    clientObj.name = createForm.inputName.value;
+    clientObj.lastName = createForm.inputLastName.value;
+    clientObj.surName = createForm.inputSurName.value;
+    clientObj.birthDay = createForm.inputBirthDay.value;
+    clientObj.gender = genderValue;
+    clientObj.education = educationValue;
+
+    clientObj.contacts = contacts;
+
+    console.log(clientObj);
+  });
+
+  createForm.modalClose.addEventListener("click", () => {
+    modal.remove();
+  });
+
+  document.addEventListener("click", (e) => {
+    if (e.target == modal) {
+      modal.remove();
+    }
+  });
+
+  return modal;
+};
